28d36b07887af9c68b169cc76b482517a3a3af7a
unknown <juanpablo.ferrari@BUEMRM-DW70365.la.corp.ipgnetwork.com>
2015-05-18 18:05:17 -0300

    pre login para chat

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var nicks = [];
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
 });
 
 
-io.on('connection', function (socket){
-	socket.on('chat message', function(msg){
-		socket.emit('chat message', msg)
+io.on('connection', function(socket) {
+
+	socket.on("nick", function(nick) {
+		
+		nicks.push(nick);
+		//socket.emit("nicks", nicks);
+
+		socket.on('chat message', function(msg) {			
+			socket.emit('chat message',nick,msg)
+		});
+
+		/*socket.on("disconnect", function() {
+			nicks.splice(nicks.indexOf(nick), 1);
+			socket.emit("nicks", nicks);
+		})*/
+
 	});
 
-	
+
+
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function() {
+	console.log('listening on *:3000');
 });
-
-
-
